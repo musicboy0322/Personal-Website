4e465f8c974af0fa971de483b2b4a7534ea9e52d
musicboy0322 <plusultra0322@gmail.com>
2026-06-22 11:23:24 -0400

    update resume

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -80,7 +80,7 @@ export default function Sidebar() {
           <li className="flex items-center gap-3">
             <FileText size={18} className="text-[#06B6D4]" />
             <a
-              href="https://drive.google.com/file/d/1a_avnxFZCDcHHj-y6D8P6HUN2qjRw9Yu/view?usp=sharing"
+              href="https://drive.google.com/file/d/1UWRNm9szjVr5pyJibeZ2rKOuwrhuIjov/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
               className="text-sm hover:text-[#22D3EE] transition"
