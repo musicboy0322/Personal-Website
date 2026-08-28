342b68b8945631d190d1230c28f4a09e3ee93f12
musicboy0322 <plusultra0322@gmail.com>
2026-08-28 10:32:01 -0400

    add work experience detail

--- components/experienceSection/components/WorkExperience.tsx
+++ components/experienceSection/components/WorkExperience.tsx
@@ -14,6 +14,7 @@ export default function ProfessionalExperienceSection() {
       projects: [
         {
           bullets: [
+            "Incoming Fall 2026 Full-Stack Intern"
           ],
           tech: [
             { icon: "/icons/programmingLanguage/python.svg", label: "Python" },
